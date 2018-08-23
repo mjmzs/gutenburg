0cfb6bc15817e35d11da58c7a78ed6e9bd735e7f
mjmzs <atsignmjmzs@gmail.com>
2018-08-23 06:41:19 +0430

    NewFixDesign&NewFooter&ContentModify&NewForms_BackUp

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,27 +4,23 @@ const Contact = () => {
     return (
       <div>
       <span className='titleSpan mb3 shadow-2 mt4'>تماس با ما</span>
-          <div class="mw7 center bg-white ph3 pv1 shadow-2 mb3">
+          <div class="mw7 center mcontent ph3 pv1 shadow-2 mb3">
               <div class="measure center">
                 <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-                <div class="mt3">
-                   <label class="db fw6 lh-copy f6" for="email-address">
-                        Email</label>
-                        <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                <div class="cf mt3 center">
+                   <label class="db fw6 lh-copy f5" for="email-address">
+                        پست الکترونیک</label>
+                        <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="email-address"  id="email-address" />
+                   <label class="mt3 db fw6 lh-copy f5" for="full-name">
+                        نام کامل</label>
+                        <input class="tr pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="full-name"  id="full-name" />
                 </div>
-                <div class="mv3">
-                    <label class="db fw6 lh-copy f6" for="password">Password</label>
-                    <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
-                </div>
+                <label for="message" class="f5 b db mb2 mt3">پیام / درخواست
+                </label>
+                <textarea id="message" name="message" class="tr db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2" aria-describedby="comment-desc">
+                </textarea>
                 </fieldset>
-                <div class="">
-                  <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
-                </div>
-                  <div class="lh-copy mt3">
-                  <a href="#0" class="f6 link dim black db">Sign up
-                  </a>
-                  <a href="#0" class="f6 link dim black db">Forgot your password?</a>
-                </div>
+                  <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f5 dib" type="submit" value="ارسال" />
             </div>
         </div>
      </div>
